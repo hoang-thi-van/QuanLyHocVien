d72bffab579edf9ba5c27ec52bd8f7109cdc5348
nguyentai <anhtaikute96@gmail.com>
2021-09-05 22:02:44 +0700

    cômiit

--- QuanLyHocVien/Program.cs
+++ QuanLyHocVien/Program.cs
@@ -7,6 +7,7 @@ namespace QuanLyHocVien
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Xin CHao");
         }
     }
 }
